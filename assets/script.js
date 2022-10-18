6dd1443fd754b47c17a029cffd8f31e7a889c728
cmarielorber <109984761+cmarielorber@users.noreply.github.com>
2022-10-17 18:33:08 -0700

    html updates and css and js

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,61 @@
-const start_quiz = document.querySelector (".start-quiz button");
+const question = [
+    {
+        question: "Which language runs in a web browser?",
+        options: "Java",
+        option: "C",
+        options: "Python",
+        answer: "javascript",
+        correct: "d",
+    },
+    {
+        question: "What is a block of JavaScript code, that can be executed when called for?",
+        a: "an event",
+        b: "a function",
+        c: "an element",
+        d: "web API",
+        correct: "b",
+    },
+    {
+        question: "What are variables?",
+        a: "Containers for storing data",
+        b: "Can only have one or two values",
+        c: "HTML",
+        d: "margins and padding",
+        correct: "a",
+    },
+    {
+        question: "What is 'Hello'?",
+        a: "boolean; variable",
+        b: "string; object",
+        c: "number, object",
+        d: "none of the above",
+        correct: "b",
+    },
+];
+
+const start_bttn = document.querySelector (".start-bttn button");
+const quiz_box = document.querySelector(".quiz-box");
 
 
+//if start button clicked
+start_bttn.onclick = ()=>{
+    // start_quiz.classList.remove("activeStart");
+    // quiz_box.classList.add("activeQuiz");
 
+    // show quiz box - remove hide
 
+    // hide start elements - add hide
 
+    // time starts and dsplays - setinterval
 
+    // show the one question
 
-start_quiz.onclick = ()=>{
-    info_box.classList.add("activeInfo"); //show info box
 };
-// if start Quiz button clicked
-continue_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide info box
-    quiz_box.classList.add("activeQuiz"); //show quiz box
-    showQuestions(0); //calling showQestions function
-    queCounter(1); //passing 1 parameter to queCounter
-    startTimer(60); //calling startTimer function
-    startTimerLine(0); //calling startTimerLine function
-}
+
+//const result_box = document.querySelector(".result_box");
+//const time_line = document.querySelector("header .time_line");
+//const timeText = document.querySelector(".timer .time_left_txt");
+//const timeCount = document.querySelector(".timer .timer_sec");
+//const options = document.querySelector(".option-list");
 
 
