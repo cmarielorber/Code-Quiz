97619f0621deb51c29ce1abcaad2c0529a54e7c1
cmarielorber <109984761+cmarielorber@users.noreply.github.com>
2022-10-18 14:04:37 -0700

    added new java

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
-const question = [
+const quizInfo = [
     {
         question: "Which language runs in a web browser?",
-        options: "Java",
-        option: "C",
-        options: "Python",
-        answer: "javascript",
-        correct: "d",
+        a: "coffee",
+        b: "C",
+        c: "Python",
+        d: "Javascript",
+        answer: "d",
     },
     {
         question: "What is a block of JavaScript code, that can be executed when called for?",
@@ -13,7 +13,7 @@ const question = [
         b: "a function",
         c: "an element",
         d: "web API",
-        correct: "b",
+        answer: "b",
     },
     {
         question: "What are variables?",
@@ -21,45 +21,99 @@ const question = [
         b: "Can only have one or two values",
         c: "HTML",
         d: "margins and padding",
-        correct: "a",
+        answer: "a",
     },
     {
         question: "What is 'Hello'?",
-        a: "boolean; variable",
+        a: "boolean;variable",
         b: "string; object",
-        c: "number, object",
+        c: "number; object",
         d: "none of the above",
-        correct: "b",
+        answer: "b",
     },
 ];
 
-const start_bttn = document.querySelector (".start-bttn button");
+const start_bttn = document.querySelector(".start-bttn button");
+const quiz = document.getElementById('quiz')
 const quiz_box = document.querySelector(".quiz-box");
+const start_quiz = document.querySelector(".start-quiz");
+const quizTimer = document.querySelector(".timer-sec");
+const quizQuestion = document.querySelector(".question");
+const a_text = document.getElementById('.a_text')
+const b_text = document.getElementById('.b_text')
+const c_text = document.getElementById('.c_text')
+const d_text = document.getElementById('.d_text')
+const quizAnswer = document.querySelectorAll('.answer');
+const submitBtn = document.getElementById('.submit')
+
+let timer;
+let timeLeft = 60;
+let currentQuiz = 0;
+let score = 0;
+
+function startTimer() {
+    timer = setInterval(function () {
+        timeLeft--;
+        quizTimer.textContent = timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(timer); //want to call end quiz function here
+        }
+    }, 1000)
+}
 
 
-//if start button clicked
-start_bttn.onclick = ()=>{
-    // start_quiz.classList.remove("activeStart");
-    // quiz_box.classList.add("activeQuiz");
-
-    // show quiz box - remove hide
-
-    // hide start elements - add hide
-
-    // time starts and dsplays - setinterval
-
+start_bttn.onclick = () => {
+    console.log(start_quiz, quiz_box)
+    // start_quiz.classList.add("hide"); // hide start elements - add hide
+    // quiz_box.classList.remove("hide"); // show quiz box - remove hide
+    start_quiz.style.display = "none"
+    quiz_box.style.display = "block"
+    startTimer(); // time starts and dsplays - setinterval
+    loadQuiz();
     // show the one question
-
 };
 
+loadQuiz()
+function loadQuiz() {
+    deselectAnswers()
+    const currentquizInfo = quizInfo[currentQuiz];
+    quizQuestion.innerText = currentquizInfo.question;
+    a_text.innerText = currentquizInfo.a;
+    b_text.innerText = currentquizInfo.b;
+    c_text.innerText = currentquizInfo.c;
+    d_text.innerText = currentquizInfo.d;
+}
+function deselectAnswers() {
+    quizAnswer.forEach(quizAnswer => quizAnswer.checked = false)
+}
+function getSelected() {
+    let answer
+    quizAnswer.forEach(quizAnswer => {
+        if (quizAnswer.checked) {
+            answer = quizAnswer.id
+        }
+    })
+    return answer
+}
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if (answer) {
+        if (answer === quizInfo[currentQuiz].correct) {
+            score++
+        }
+        currentQuiz++
+        if (currentQuiz < quizInfo.length) {
+            loadQuiz()
+        } else {
+            quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+           <button onclick="location.reload()">Reload</button>
+           `
+        }
+    }
+})
 
-//add EVENT LISTENER FOR ANSWERS 
+// const quiz= document.getElementById('quiz')
 
-
-//const result_box = document.querySelector(".result_box");
-//const time_line = document.querySelector("header .time_line");
-//const timeText = document.querySelector(".timer .time_left_txt");
-//const timeCount = document.querySelector(".timer .timer_sec");
-//const options = document.querySelector(".option-list");
-
+// const questionEl = document.getElementById('question')
 
